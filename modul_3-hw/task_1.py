bd3edc51edb7c7596e6f7b57824db6aec4753e37
Anna <asofof2@gmail.com>
2025-03-22 22:16:44 +0200

    commit-2_task_1

--- modul_3-hw/task_1.py
+++ modul_3-hw/task_1.py
@@ -3,18 +3,22 @@ from datetime import datetime
 
 def get_days_from_today(date: str) -> int:
 # Функція приймає один параметр: date — рядок, що представляє дату у форматі 'РРРР-ММ-ДД'. Возвращает целое число
+    
+    try:
+        # Перетворіть рядок дати у форматі 'РРРР-ММ-ДД' у об'єкт datetime.
+        datetime_object = datetime.strptime(date, "%Y-%m-%d")
 
-    # Перетворіть рядок дати у форматі 'РРРР-ММ-ДД' у об'єкт datetime.
-    datetime_object = datetime.strptime(date, "%Y-%m-%d")
+        # Отримайте поточну дату, використовуючи datetime.today().
+        current_datetime = datetime.today()
 
-    # Отримайте поточну дату, використовуючи datetime.today().
-    current_datetime = datetime.today()
+        # Розрахуйте різницю між поточною датою та заданою датою.
+        difference = current_datetime - datetime_object
 
-    # Розрахуйте різницю між поточною датою та заданою датою.
-    difference = current_datetime - datetime_object
+        # Поверніть різницю у днях як ціле число.
+        return difference.days
+    except(ValueError):
+        return "Неправильний формат вхідних даних."
 
-    # Поверніть різницю у днях як ціле число.
-    return difference.days
+print(get_days_from_today("2028-10.10"))
 
-print(get_days_from_today("2028-10-10"))
 
